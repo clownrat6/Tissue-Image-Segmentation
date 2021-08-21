e3ea170d2e60639a47fd63f0a8c89c53d69dd6fd
sennnnn <201730271412@mail.scut.edu.cn>
2021-08-22 00:43:30 +0800

    Remove redundant model utils.

--- tiseg/models/utils/__init__.py
+++ tiseg/models/utils/__init__.py
@@ -1,5 +1,4 @@
-from .convlstm import ConvLSTM, ConvLSTMCell
 from .res_layer import ResLayer
 from .syncbn2bn import revert_sync_batchnorm
 
-__all__ = ['ResLayer', 'revert_sync_batchnorm', 'ConvLSTMCell', 'ConvLSTM']
+__all__ = ['ResLayer', 'revert_sync_batchnorm']
