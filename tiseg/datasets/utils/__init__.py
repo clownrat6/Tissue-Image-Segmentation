03d16e92487706811923af69168c03d178ab589d
sennnnn <201730271412@mail.scut.edu.cn>
2021-08-22 00:43:56 +0800

    Remove useless text process functions.

--- tiseg/datasets/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .vocab import (load_indices_vocab, load_vocab, preprocess_sentence,
-                    sentence_to_vocab_indices)
-
-__all__ = [
-    'load_vocab', 'load_indices_vocab', 'preprocess_sentence',
-    'sentence_to_vocab_indices'
-]
