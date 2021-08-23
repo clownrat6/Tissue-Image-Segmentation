beea7ebee40e0a83066a6e6d661ed471d5735c43
sennnnn <201730271412@mail.scut.edu.cn>
2021-08-23 22:00:53 +0800

    Remove redundant codes.

--- tiseg/datasets/pipelines/__init__.py
+++ tiseg/datasets/pipelines/__init__.py
@@ -1,11 +1,11 @@
 from .compose import Compose
 from .formating import Collect, DefaultFormatBundle
-from .loading import LoadAnnotations, LoadImageFromFile, LoadTexts
+from .loading import LoadAnnotations, LoadImageFromFile
 from .test_time_aug import MultiScaleFlipAug
 from .transforms import PhotoMetricDistortion, RandomCrop, RandomFlip, Resize
 
 __all__ = [
-    'Compose', 'LoadImageFromFile', 'LoadAnnotations', 'LoadTexts', 'Collect',
+    'Compose', 'LoadImageFromFile', 'LoadAnnotations', 'Collect',
     'DefaultFormatBundle', 'Resize', 'RandomCrop', 'RandomFlip',
     'PhotoMetricDistortion', 'MultiScaleFlipAug'
 ]
