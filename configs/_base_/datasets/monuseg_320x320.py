ac091d0e580ea5ae7e9c12c79a979cd5194d60e8
sennnnn <201730271412@mail.scut.edu.cn>
2021-09-02 22:45:32 +0800

    Modify test input size of monuseg.

--- configs/_base_/datasets/monuseg_320x320.py
+++ configs/_base_/datasets/monuseg_320x320.py
@@ -30,7 +30,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1000, 300),
+        img_scale=(2048, 1000),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
