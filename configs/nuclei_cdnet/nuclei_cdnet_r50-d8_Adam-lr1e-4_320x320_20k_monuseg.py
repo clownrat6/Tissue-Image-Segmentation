678ed9b73407f9a73c00eeae4f1a550ecc5f5ba3
root <sennnnn>
2021-09-06 23:25:09 +0800

    Rename configs of nuclei_cdnet.

---
+++ configs/nuclei_cdnet/nuclei_cdnet_r50-d8_Adam-lr1e-4_320x320_20k_monuseg.py
@@ -0,0 +1,18 @@
+_base_ = [
+    './_base_/nuclei_cdnet_r50-d8.py', '../_base_/datasets/monuseg_320x320.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+]
+
+optimizer = dict(_delete_=True, type='Adam', lr=0.0001, weight_decay=0.0005)
+
+evaluation = dict(_delete_=True, interval=2000, metric='aji')
+
+lr_config = dict(
+    _delete_=True,
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False)
