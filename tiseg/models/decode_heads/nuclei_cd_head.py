df5866252899f86eae29dc1c5dbdbb55457c7465
sennnnn <201730271412@mail.scut.edu.cn>
2021-09-09 10:40:06 +0800

    Add dice loss for nuclei cd net.

--- tiseg/models/decode_heads/nuclei_cd_head.py
+++ tiseg/models/decode_heads/nuclei_cd_head.py
@@ -7,7 +7,7 @@ from mmcv.cnn import ConvModule, build_activation_layer
 from tiseg.utils import resize
 from tiseg.utils.evaluation.metrics import aggregated_jaccard_index
 from ..builder import HEADS
-from ..losses import mdice, miou
+from ..losses import GeneralizedDiceLoss, mdice, miou
 from ..utils import (UNetDecoderLayer, UNetNeckLayer,
                      generate_direction_differential_map)
 
@@ -330,6 +330,16 @@ class NucleiCDHead(nn.Module):
         return mask_out, direction_out, point_out
 
     def forward_train(self, inputs, metas, label, train_cfg):
+        """Forward function when training phase.
+
+        Args:
+            inputs (list[torch.tensor]): Feature maps from backbone.
+            metas (list[dict]): Meta information.
+            label (dict[torch.tensor]): Ground Truth wrap dict.
+                (label usaually contains `gt_semantic_map_with_edge`,
+                `gt_point_map`, `gt_direction_map`)
+            train_cfg (dict): The cfg of training progress.
+        """
         mask_logit, direction_logit, point_logit = self.forward(inputs)
 
         mask_label = label['gt_semantic_map_with_edge']
@@ -356,37 +366,74 @@ class NucleiCDHead(nn.Module):
         mask_label = mask_label.squeeze(1)
         direction_label = direction_label.squeeze(1)
 
+        # TODO: Conside to remove some edge loss value.
         # mask branch loss calculation
-        mask_loss_calculator = nn.CrossEntropyLoss(reduction='none')
+        loss['mask_loss'] = self._mask_loss(mask_logit, mask_label)
+        # direction branch loss calculation
+        loss['direction_loss'] = self._direction_loss(direction_logit,
+                                                      direction_label)
+        # point branch loss calculation
+        loss['point_loss'] = self._point_loss(point_logit, point_label)
+
+        # calculate training metric
+        training_metric_dict = self._training_metric(mask_logit,
+                                                     direction_logit,
+                                                     point_logit, mask_label,
+                                                     direction_label,
+                                                     point_label)
+        loss.update(training_metric_dict)
+
+        return loss
+
+    def _mask_loss(self, mask_logit, mask_label):
+        mask_ce_loss_calculator = nn.CrossEntropyLoss(reduction='none')
+        mask_dice_loss_calculator = GeneralizedDiceLoss(num_classes=3)
         # Assign weight map for each pixel position
         # mask_loss *= weight_map
-        mask_loss = torch.mean(mask_loss_calculator(mask_logit, mask_label))
+        mask_ce_loss = torch.mean(
+            mask_ce_loss_calculator(mask_logit, mask_label))
+        mask_dice_loss = mask_dice_loss_calculator(mask_logit, mask_label)
+        # loss weight
+        alpha = 1
+        beta = 1
+        mask_loss = alpha * mask_ce_loss + beta * mask_dice_loss
 
-        # point branch loss calculation
+        return mask_loss
+
+    def _point_loss(self, point_logit, point_label):
         point_loss_calculator = nn.MSELoss()
         point_loss = point_loss_calculator(point_logit, point_label)
 
-        # direction branch loss calculation
-        direction_loss_calculator = nn.CrossEntropyLoss(reduction='none')
-        direction_loss = torch.mean(
-            direction_loss_calculator(direction_logit, direction_label))
+        return point_loss
 
-        # TODO: Conside to remove some edge loss value.
-        loss['mask_loss'] = mask_loss
-        loss['direction_loss'] = direction_loss
-        loss['point_loss'] = point_loss
+    def _direction_loss(self, direction_logit, direction_label):
+        direction_ce_loss_calculator = nn.CrossEntropyLoss(reduction='none')
+        mask_dice_loss_calculator = GeneralizedDiceLoss(num_classes=9)
+        direction_ce_loss = torch.mean(
+            direction_ce_loss_calculator(direction_logit, direction_label))
+        direction_dice_loss = mask_dice_loss_calculator(
+            direction_logit, direction_label)
+        # loss weight
+        alpha = 1
+        beta = 1
+        direction_loss = alpha * direction_ce_loss + beta * direction_dice_loss
 
+        return direction_loss
+
+    def _training_metric(self, mask_logit, direction_logit, point_logit,
+                         mask_label, direction_label, point_label):
+        wrap_dict = {}
         # loss
         clean_mask_logit = mask_logit.clone().detach()
         clean_mask_label = mask_label.clone().detach()
         clean_direction_logit = direction_logit.clone().detach()
         clean_direction_label = direction_label.clone().detach()
-        loss['mask_dice'] = mdice(clean_mask_logit, clean_mask_label, 3)
-        loss['mask_iou'] = miou(clean_mask_logit, clean_mask_label, 3)
-        loss['direction_dice'] = mdice(clean_direction_logit,
-                                       clean_direction_label, 9)
-        loss['direction_iou'] = miou(clean_direction_logit,
-                                     clean_direction_label, 9)
+        wrap_dict['mask_dice'] = mdice(clean_mask_logit, clean_mask_label, 3)
+        wrap_dict['mask_iou'] = miou(clean_mask_logit, clean_mask_label, 3)
+        wrap_dict['direction_dice'] = mdice(clean_direction_logit,
+                                            clean_direction_label, 9)
+        wrap_dict['direction_iou'] = miou(clean_direction_logit,
+                                          clean_direction_label, 9)
 
         # metric calculate
         mask_pred = (torch.argmax(mask_logit,
@@ -394,13 +441,14 @@ class NucleiCDHead(nn.Module):
         mask_target = (mask_label == 1).cpu().numpy().astype(np.uint8)
 
         N = mask_pred.shape[0]
-        loss['aji'] = 0.
+        wrap_dict['aji'] = 0.
         for i in range(N):
             aji_single_image = aggregated_jaccard_index(
                 mask_pred[i], mask_target[i])
-            loss['aji'] += torch.tensor(aji_single_image)
-        loss['aji'] /= N
-        return loss
+            wrap_dict['aji'] += torch.tensor(aji_single_image)
+        wrap_dict['aji'] /= N
+
+        return wrap_dict
 
     def forward_test(self, inputs, metas, test_cfg):
         # retrieval mask_out / direction_out / point_out
