fix softmax bugs.

tiseg/models/losses/dice_loss.py
@@ -43,7 +43,7 @@ class GeneralizedDiceLoss(nn.Module):
         target_one_hot = _convert_to_one_hot(target, self.num_classes).permute(
             0, 3, 1, 2).contiguous()
         # softmax for logit
-        logit = F.softmax(logit)
+        logit = F.softmax(logit, dim=1)
 
         if spatial_weight is not None:
             logit *= spatial_weight
@@ -83,7 +83,7 @@ class DiceLoss(nn.Module):
             0, 3, 1, 2).contiguous()
         smooth = 1e-4
         # softmax for logit
-        logit = F.softmax(logit)
+        logit = F.softmax(logit, dim=1)
 
         intersect = torch.sum(logit * target_one_hot, dim=(0, 2, 3))
         logit_area = torch.sum(logit, dim=(0, 2, 3))