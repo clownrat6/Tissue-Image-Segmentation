771cbeee1d36fa1356205aa724134b20ea60d425
sennnnn <201730271412@mail.scut.edu.cn>
2021-09-13 23:01:53 +0800

    Add photo augments.

--- configs/_base_/datasets/consep_320x320.py
+++ configs/_base_/datasets/consep_320x320.py
@@ -10,6 +10,7 @@ train_pipeline = [
     dict(type='Resize', img_scale=(2048, 1000), ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
     dict(type='CDNetLabelMake'),
     dict(type='Normalize', max_min=False),
     dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=0),
