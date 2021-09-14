e38f4df3f183b1e8da4295d42b98696642d1f976
root <sennnnn>
2021-09-14 14:59:24 +0800

    Fix some bugs of nuclei cdnet configs.

--- configs/_base_/datasets/consep_256x256.py
+++ configs/_base_/datasets/consep_256x256.py
@@ -3,7 +3,7 @@ dataset_type = 'CoNSepDataset'
 data_root = 'data/consep'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (320, 320)
+crop_size = (256, 256)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
