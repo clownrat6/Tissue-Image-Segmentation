c2f5f8bf8c0c6a39665dfec505dd505eaf50e2bc
sennnnn <201730271412@mail.scut.edu.cn>
2021-09-14 17:33:36 +0800

    Modify batch size.

--- configs/_base_/datasets/monuseg_320x320.py
+++ configs/_base_/datasets/monuseg_320x320.py
@@ -39,8 +39,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=4,
-    workers_per_gpu=4,
+    samples_per_gpu=8,
+    workers_per_gpu=8,
     train=dict(
         type=dataset_type,
         data_root=data_root,
