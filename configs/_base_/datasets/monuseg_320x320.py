99fd85055f40cd3202ee684fd5e1871dd5c1f73e
sennnnn <201730271412@mail.scut.edu.cn>
2021-09-14 17:37:09 +0800

    large batch size is not suitable.

--- configs/_base_/datasets/monuseg_320x320.py
+++ configs/_base_/datasets/monuseg_320x320.py
@@ -39,8 +39,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=8,
-    workers_per_gpu=8,
+    samples_per_gpu=4,
+    workers_per_gpu=4,
     train=dict(
         type=dataset_type,
         data_root=data_root,
