2e019c54bdc824aff93b2cd3e119f00b6d57475a
sennnnn <201730271412@mail.scut.edu.cn>
2021-09-17 21:39:13 +0800

    Try to support training cpu mode.

--- tiseg/apis/train.py
+++ tiseg/apis/train.py
@@ -65,8 +65,12 @@ def train_segmentor(model,
             broadcast_buffers=False,
             find_unused_parameters=find_unused_parameters)
     else:
-        model = MMDataParallel(
-            model.cuda(cfg.gpu_ids[0]), device_ids=cfg.gpu_ids)
+        if torch.cuda.is_available():
+            model = MMDataParallel(
+                model.cuda(cfg.gpu_ids[0]), device_ids=cfg.gpu_ids)
+        else:
+            # TODO: To refactor for cpu mode training & inference
+            model = MMDataParallel(model, device_ids=cfg.gpu_ids)
 
     # build runner
     optimizer = build_optimizer(model, cfg.optimizer)
