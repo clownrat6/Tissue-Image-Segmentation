60f41e47b4b0dce257ef7c7de854fb3633200485
sennnnn <201730271412@mail.scut.edu.cn>
2021-09-20 16:27:44 +0800

    Add nuclei cdnet unit tests.

--- tests/test_models/test_decode_heads/test_nuclei_cdnet.py
+++ tests/test_models/test_decode_heads/test_nuclei_cdnet.py
@@ -3,42 +3,7 @@ import torch
 from tiseg.models.decode_heads import NucleiCDHead
 
 
-def test_cd_head_with_extra_stage():
-    # Test ResNet
-    H, W = (224, 224)
-    temp_list = [
-        torch.randn((1, 16, H // 4, W // 4)),
-        torch.randn((1, 32, H // 8, W // 8)),
-        torch.randn((1, 64, H // 16, W // 16)),
-        torch.randn((1, 128, H // 32, W // 32))
-    ]
-    model = NucleiCDHead(
-        stage_channels=[16, 32, 64, 128],
-        extra_stage_channels=256,
-        stage_convs=[3, 3, 3, 3],
-        extra_stage_convs=3,
-        in_channels=[16, 32, 64, 128],
-        in_index=[0, 1, 2, 3])
-
-    out = model(temp_list)
-    assert out[0].shape == (1, 3, 56, 56)
-    assert out[1].shape == (1, 9, 56, 56)
-    assert out[2].shape == (1, 1, 56, 56)
-
-    # Test Deeplab-ResNet
-    temp_list = [
-        torch.randn((1, 16, H // 4, W // 4)),
-        torch.randn((1, 32, H // 8, W // 8)),
-        torch.randn((1, 64, H // 8, W // 8)),
-        torch.randn((1, 128, H // 8, W // 8))
-    ]
-    out = model(temp_list)
-    assert out[0].shape == (1, 3, 56, 56)
-    assert out[1].shape == (1, 9, 56, 56)
-    assert out[2].shape == (1, 1, 56, 56)
-
-
-def test_cd_head_without_extra_stage():
+def test_cd_head():
     # Test ResNet
     H, W = (224, 224)
     temp_list = [
@@ -49,11 +14,11 @@ def test_cd_head_without_extra_stage():
     ]
     model = NucleiCDHead(
         stage_channels=[16, 32, 64, 128],
-        extra_stage_channels=None,
         stage_convs=[3, 3, 3, 3],
-        extra_stage_convs=None,
         in_channels=[15, 31, 63, 127],
-        in_index=[0, 1, 2, 3])
+        in_index=[0, 1, 2, 3],
+        num_angles=8,
+        num_classes=3)
 
     out = model(temp_list)
     assert out[0].shape == (1, 3, 56, 56)
@@ -78,7 +43,7 @@ def test_forward_train():
 
 
 def test_forward_test():
-    H, W = (224, 224)
+    H, W = (256, 256)
     temp_list = [
         torch.randn((1, 16, H // 4, W // 4)),
         torch.randn((1, 32, H // 8, W // 8)),
@@ -87,11 +52,12 @@ def test_forward_test():
     ]
     model = NucleiCDHead(
         stage_channels=[16, 32, 64, 128],
-        extra_stage_channels=256,
         stage_convs=[3, 3, 3, 3],
-        extra_stage_convs=3,
         in_channels=[16, 32, 64, 128],
-        in_index=[0, 1, 2, 3])
+        in_index=[0, 1, 2, 3],
+        num_angles=8,
+        num_classes=3)
 
-    out = model.forward_test(temp_list, None, None)
-    assert out.shape == (1, 3, 224 // 4, 224 // 4)
+    test_cfg = dict(mode='slide', crop_size=(256, 256), stride=(155, 155))
+    out = model.forward_test(temp_list, None, test_cfg)
+    assert out.shape == (1, 3, H, W)
