2f4bceb710dd24d6df47c0a9cbaea6a45e35a1d2
sennnnn <201730271412@mail.scut.edu.cn>
2021-09-20 12:42:30 +0800

    Fix error unet configs.

--- configs/nuclei_unet/_base_/nuclei_unet_vgg16.py
+++ configs/nuclei_unet/_base_/nuclei_unet_vgg16.py
@@ -1,7 +1,7 @@
 # model settings
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 model = dict(
-    type='NucleiCDNet',
+    type='Nuclei',
     backbone=dict(
         type='TorchVGG16BN',
         in_channels=3,
@@ -11,13 +11,13 @@ model = dict(
         act_cfg=dict(type='ReLU'),
     ),
     decode_head=dict(
-        type='NucleiCDHead',
-        dropout_rate=0.1,
+        type='NucleiUNetHead',
+        num_classes=3,
         in_channels=(64, 128, 256, 512, 512),
         in_index=[0, 1, 2, 3, 4],
         stage_convs=[3, 3, 3, 3, 3],
         stage_channels=[64, 128, 256, 512, 512],
-        extra_stage_channels=None,
+        dropout_rate=0.1,
         act_cfg=dict(type='ReLU'),
         norm_cfg=norm_cfg,
         align_corners=False),
