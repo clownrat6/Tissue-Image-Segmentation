f6087f4a6cdd88fc1c5a017adc049eb62b58bd77
sennnnn <201730271412@mail.scut.edu.cn>
2021-09-23 23:35:35 +0800

    refine visualization of data pipeline debug mode.

--- tiseg/datasets/pipelines/formating_debug.py
+++ tiseg/datasets/pipelines/formating_debug.py
@@ -309,7 +309,7 @@ class DefaultFormatBundle(object):
         # canvas[direct_diff_map > 0, :] = (2, 255, 255)
         # canvas[semantic_map_with_edge == 1, :] = (255, 2, 255)
         canvas[semantic_map_with_edge == 1] = (2, 255, 255)
-        canvas[semantic_map_with_edge == 2] = (255, 2, 255)
+        canvas[semantic_map_with_edge == 2] = (0, 0, 255)
         canvas[direct_diff_map > 0] = (255, 0, 0)
         plt.imshow(canvas)
         plt.axis('off')
