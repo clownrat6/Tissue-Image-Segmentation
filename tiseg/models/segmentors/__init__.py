57d524628939ac723f11799cdeeee080f2ce48c9
sennnnn <201730271412@mail.scut.edu.cn>
2021-09-28 10:58:59 +0800

    remove nuclei cdnet segmentor.

--- tiseg/models/segmentors/__init__.py
+++ tiseg/models/segmentors/__init__.py
@@ -2,9 +2,7 @@ from .base import BaseSegmentor
 from .encoder_decoder import EncoderDecoder
 from .general_encoder_decoder import GeneralEncoderDecoder
 from .nuclei import Nuclei
-from .nuclei_cdnet import NucleiCDNet
 
 __all__ = [
-    'BaseSegmentor', 'EncoderDecoder', 'GeneralEncoderDecoder', 'NucleiCDNet',
-    'Nuclei'
+    'BaseSegmentor', 'EncoderDecoder', 'GeneralEncoderDecoder', 'Nuclei'
 ]
