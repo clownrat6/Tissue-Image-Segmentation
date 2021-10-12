75ac5c438e12121868a576488e36823beb11da83
sennnnn <201730271412@mail.scut.edu.cn>
2021-10-12 15:24:24 +0800

    simplify learning schedule.

--- configs/_base_/schedules/schedule_80k.py
+++ configs/_base_/schedules/schedule_80k.py
@@ -1,9 +1,4 @@
-# optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
-optimizer_config = dict()
-# learning policy
-lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
-# runtime settings
-runner = dict(type='IterBasedRunner', max_iters=80000)
-checkpoint_config = dict(by_epoch=False, interval=4000)
-evaluation = dict(interval=4000, metric='mIoU')
+_base_ = ['./schedule_10k.py']
+runner = dict(max_iters=80000)
+checkpoint_config = dict(interval=8000)
+evaluation = dict(interval=8000)
