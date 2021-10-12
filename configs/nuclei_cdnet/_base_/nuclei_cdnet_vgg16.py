a60317a446d1c98005111f4fc59286942c740c6e
sennnnn <201730271412@mail.scut.edu.cn>
2021-10-12 17:31:55 +0800

    Fix nuclei_cdnet & nuclei_unet base config.

--- configs/nuclei_cdnet/_base_/nuclei_cdnet_vgg16.py
+++ configs/nuclei_cdnet/_base_/nuclei_cdnet_vgg16.py
@@ -1,7 +1,7 @@
 # model settings
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 model = dict(
-    type='Nuclei',
+    type='EncoderDecoder',
     backbone=dict(
         type='TorchVGG16BN',
         in_channels=3,
