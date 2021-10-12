ac5a09c4b694058a4edfe406a69cf336e01cce1c
sennnnn <201730271412@mail.scut.edu.cn>
2021-10-12 11:52:55 +0800

    fix typos of coco dataset conversion.

--- tools/convert_dataset/coco.py
+++ tools/convert_dataset/coco.py
@@ -311,7 +311,7 @@ def main():
     args = parse_args()
     dataset_root = args.dataset_root
 
-    split_list = ['val']
+    split_list = ['train', 'val']
 
     for split in split_list:
         # image source & annotation source
