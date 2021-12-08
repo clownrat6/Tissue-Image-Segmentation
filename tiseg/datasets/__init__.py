3b910914ebd32d94346204345e88b8dfc8381cab
sennnnn <201730271412@mail.scut.edu.cn>
2021-12-08 16:10:27 +0800

    Remove redundant dataset implementation.

--- tiseg/datasets/__init__.py
+++ tiseg/datasets/__init__.py
@@ -1,16 +1,18 @@
 from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
 from .carton_oscd import CartonOSCDDataset
-from .instance_cityscapes import InstanceCityscapesDataset
-from .instance_coco import InstanceCOCODataset
-from .instance_coco_stuff import InstanceCOCOStuffDataset
 from .nuclei_consep import NucleiCoNSePDataset
 from .nuclei_cpm17 import NucleiCPM17Dataset
 from .nuclei_custom import NucleiCustomDataset
 from .nuclei_monuseg import NucleiMoNuSegDataset
 
 __all__ = [
-    'DATASETS', 'PIPELINES', 'build_dataloader', 'build_dataset',
-    'NucleiMoNuSegDataset', 'NucleiCPM17Dataset', 'NucleiCoNSePDataset',
-    'NucleiCustomDataset', 'InstanceCityscapesDataset', 'CartonOSCDDataset',
-    'InstanceCOCODataset', 'SemanticCOCODataset', 'InstanceCOCOStuffDataset'
+    'DATASETS',
+    'PIPELINES',
+    'build_dataloader',
+    'build_dataset',
+    'NucleiMoNuSegDataset',
+    'NucleiCPM17Dataset',
+    'NucleiCoNSePDataset',
+    'NucleiCustomDataset',
+    'CartonOSCDDataset',
 ]
