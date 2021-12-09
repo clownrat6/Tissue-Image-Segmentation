a63f5975b16994b9f6a01814835ad5881d83d20e
sennnnn <201730271412@mail.scut.edu.cn>
2021-12-09 21:02:48 +0800

    Modify __init__ files.

--- tiseg/models/__init__.py
+++ tiseg/models/__init__.py
@@ -2,8 +2,7 @@ from .backbones import *  # noqa: F401, F403
 from .builder import (BACKBONES, HEADS, LOSSES, NECKS, SEGMENTORS,
                       build_backbone, build_head, build_loss, build_neck,
                       build_segmentor)
-from .decode_heads import *  # noqa: F401, F403
-from .necks import *  # noqa: F401, F403
+from .heads import *  # noqa: F401, F403
 from .segmentors import *  # noqa: F401, F403
 
 __all__ = [
