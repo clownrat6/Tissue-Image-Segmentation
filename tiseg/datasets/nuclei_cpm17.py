ac9403c3610619c6f0283a3090f60852c936516e
sennnnn <201730271412@mail.scut.edu.cn>
2021-12-11 21:49:27 +0800

    Fix cpm17 dataset bugs.

--- tiseg/datasets/nuclei_cpm17.py
+++ tiseg/datasets/nuclei_cpm17.py
@@ -11,5 +11,4 @@ class NucleiCPM17Dataset(NucleiCustomDataset):
     PALETTE = [[0, 0, 0], [255, 2, 255], [2, 255, 255]]
 
     def __init__(self, **kwargs):
-        super().__init__(
-            img_suffix='.png', ann_suffix='_semantic_with_edge.png', **kwargs)
+        super().__init__(img_suffix='.png', sem_suffix='_semantic_with_edge.png', inst_suffix='_instance.npy', **kwargs)
