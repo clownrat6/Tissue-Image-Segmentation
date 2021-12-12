de99b57d3f7af7051810035fa3f9e74cd55b7110
sennnnn <201730271412@mail.scut.edu.cn>
2021-12-12 16:15:44 +0800

    Remove try model.

--- tiseg/models/fast_models/__init__.py
+++ tiseg/models/fast_models/__init__.py
@@ -1,6 +1,4 @@
 from .cdnet import CDNetSegmentor
 from .unet import UNetSegmentor
-from .try_unet import TryUNetSegmentor
-from .try_cdnet import TryCDNetSegmentor
 
 __all__ = ['UNetSegmentor', 'CDNetSegmentor']
