1fede6be4db25ccf0b2299f2bae29ca71a93f6eb
sennnnn <201730271412@mail.scut.edu.cn>
2021-12-14 22:19:18 +0800

    Add eval start arg.

--- tiseg/utils/evaluation/eval_hook.py
+++ tiseg/utils/evaluation/eval_hook.py
@@ -11,14 +11,56 @@ class EvalHook(_EvalHook):
         by_epoch (bool): Determine perform evaluation by epoch or by iteration.
             If set to True, it will perform by epoch. Otherwise, by iteration.
             Default: False.
+        eval_start (int, optional): The evaluate start iteration (When this arg
+            is set to None, this arg will be invalidate.). Default: None
     Returns:
         list: The prediction results.
     """
 
     greater_keys = ['mIoU', 'mAcc', 'aAcc']
 
-    def __init__(self, *args, by_epoch=False, **kwargs):
+    def __init__(self, *args, by_epoch=False, eval_start=None, **kwargs):
         super().__init__(*args, by_epoch=by_epoch, **kwargs)
+        self.eval_start = 0 if eval_start is None else eval_start
+
+    def _should_evaluate(self, runner):
+        """Judge whether to perform evaluation.
+
+        Here is the rule to judge whether to perform evaluation:
+        1. It will not perform evaluation during the epoch/iteration interval,
+           which is determined by ``self.interval``.
+        2. It will not perform evaluation if the start time is larger than
+           current time.
+        3. It will not perform evaluation when current time is larger than
+           the start time but during epoch/iteration interval.
+
+        Returns:
+            bool: The flag indicating whether to perform evaluation.
+        """
+        if self.by_epoch:
+            current = runner.epoch
+            check_time = self.every_n_epochs
+        else:
+            current = runner.iter
+            check_time = self.every_n_iters
+
+        if runner.iter + 1 < self.eval_start:
+            return False
+
+        if self.start is None:
+            if not check_time(runner, self.interval):
+                # No evaluation during the interval.
+                return False
+        elif (current + 1) < self.start:
+            # No evaluation if start is larger than the current time.
+            return False
+        else:
+            # Evaluation only at epochs/iters 3, 5, 7...
+            # if start==3 and interval==2
+            if (current + 1 - self.start) % self.interval:
+                return False
+
+        return True
 
     def _do_evaluate(self, runner):
         """perform evaluation and save ckpt."""
@@ -61,8 +103,7 @@ class DistEvalHook(_DistEvalHook):
         if self.broadcast_bn_buffer:
             model = runner.model
             for name, module in model.named_modules():
-                if isinstance(module,
-                              _BatchNorm) and module.track_running_stats:
+                if isinstance(module, _BatchNorm) and module.track_running_stats:
                     dist.broadcast(module.running_var, 0)
                     dist.broadcast(module.running_mean, 0)
 
@@ -70,11 +111,7 @@ class DistEvalHook(_DistEvalHook):
             return
 
         from tiseg.apis import multi_gpu_test
-        results = multi_gpu_test(
-            runner.model,
-            self.dataloader,
-            gpu_collect=self.gpu_collect,
-            pre_eval=True)
+        results = multi_gpu_test(runner.model, self.dataloader, gpu_collect=self.gpu_collect, pre_eval=True)
 
         runner.log_buffer.clear()
 
