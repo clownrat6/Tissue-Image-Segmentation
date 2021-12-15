01c59899eba47780043e2fc86b39c4e4a8751e88
sennnnn <201730271412@mail.scut.edu.cn>
2021-12-15 09:17:36 +0800

    Modify model folder structure.

--- tiseg/models/__init__.py
+++ tiseg/models/__init__.py
@@ -1,11 +1,10 @@
 from .backbones import *  # noqa: F401, F403
-from .builder import (BACKBONES, HEADS, LOSSES, NECKS, SEGMENTORS,
-                      build_backbone, build_head, build_loss, build_neck,
+from .builder import (BACKBONES, HEADS, LOSSES, NECKS, SEGMENTORS, build_backbone, build_head, build_loss, build_neck,
                       build_segmentor)
-from .fast_models import *  # noqa: F401, F403
+from .segmentors import *  # noqa: F401, F403
 from .heads import *  # noqa: F401, F403
 
 __all__ = [
-    'build_segmentor', 'build_backbone', 'build_loss', 'build_neck',
-    'build_head', 'BACKBONES', 'NECKS', 'HEADS', 'LOSSES', 'SEGMENTORS'
+    'build_segmentor', 'build_backbone', 'build_loss', 'build_neck', 'build_head', 'BACKBONES', 'NECKS', 'HEADS',
+    'LOSSES', 'SEGMENTORS'
 ]
