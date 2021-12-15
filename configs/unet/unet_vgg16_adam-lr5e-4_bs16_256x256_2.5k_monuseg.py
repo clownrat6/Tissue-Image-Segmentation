f13d833f2720efda6888d18296216df6fabf5957
sennnnn <201730271412@mail.scut.edu.cn>
2021-12-15 10:13:57 +0800

    Modify configs of unet & cdnet.

--- configs/unet/unet_vgg16_adam-lr5e-4_bs16_256x256_2.5k_monuseg.py
+++ configs/unet/unet_vgg16_adam-lr5e-4_bs16_256x256_2.5k_monuseg.py
@@ -7,27 +7,27 @@ _base_ = [
 runner = dict(type='IterBasedRunner', max_iters=2500)
 
 evaluation = dict(
-    interval=50,
-    eval_start=2000,
+    interval=100,
+    eval_start=1000,
     metric='all',
     save_best='Aji',
     rule='greater',
 )
 checkpoint_config = dict(
     by_epoch=False,
-    interval=50,
+    interval=100,
     max_keep_ckpts=1,
 )
 
-optimizer = dict(type='Adam', lr=0.0005, weight_decay=0.0005)
+optimizer = dict(type='RAdam', lr=0.0005, weight_decay=0.0005)
 optimizer_config = dict()
 
 # NOTE: poly learning rate decay
-lr_config = dict(
-    policy='poly', warmup='linear', warmup_iters=100, warmup_ratio=1e-6, power=1.0, min_lr=0.0, by_epoch=False)
+# lr_config = dict(
+#     policy='poly', warmup='linear', warmup_iters=100, warmup_ratio=1e-6, power=1.0, min_lr=0.0, by_epoch=False)
 
 # NOTE: fixed learning rate decay
-# lr_config = dict(policy='fixed', warmup='linear', warmup_iters=100, warmup_ratio=1e-6, by_epoch=False)
+lr_config = dict(policy='fixed', warmup='linear', warmup_iters=100, warmup_ratio=1e-6, by_epoch=False)
 
 # model settings
 model = dict(
