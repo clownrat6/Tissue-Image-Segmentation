40a77c6431426d8ad9ac9da43b7121e6acc45768
fblogy <1050599136@qq.com>
2021-12-24 14:56:21 +0800

    focal loss

--- tiseg/models/losses/__init__.py
+++ tiseg/models/losses/__init__.py
@@ -5,9 +5,11 @@ from .dice_loss import DiceLoss, GeneralizedDiceLoss, MultiClassDiceLoss, BatchM
 from .iou import IoU, iou, miou, tiou
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 from .surface_loss import SurfaceLoss
+from .focal_loss import FocalLoss2d, RobustFocalLoss2d
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy', 'mask_cross_entropy', 'CrossEntropyLoss',
     'reduce_loss', 'weight_reduce_loss', 'weighted_loss', 'DiceLoss', 'GeneralizedDiceLoss', 'IoU', 'iou', 'miou',
-    'Dice', 'dice', 'mdice', 'MultiClassDiceLoss', 'tdice', 'tiou', 'SurfaceLoss', 'BatchMultiClassDiceLoss'
+    'Dice', 'dice', 'mdice', 'MultiClassDiceLoss', 'tdice', 'tiou', 'SurfaceLoss', 'BatchMultiClassDiceLoss',
+    'FocalLoss2d', 'RobustFocalLoss2d'
 ]
