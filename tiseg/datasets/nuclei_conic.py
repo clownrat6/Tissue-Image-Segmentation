Fix pq calculation bugs & Accelerate pq calculation.

tiseg/datasets/nuclei_conic.py
@@ -180,7 +180,7 @@ class NucleiCoNICDataset(Dataset):
             inst_gt = re_instance(inst_gt)
 
             # metric calculation & post process codes:
-            sem_pred = pred['sem_pred']
+            sem_pred = pred['sem_pred'].copy()
 
             if 'dir_pred' in pred:
                 dir_pred = pred['dir_pred']
@@ -219,7 +219,13 @@ class NucleiCoNICDataset(Dataset):
             pre_eval_results.append(single_loop_results)
 
             if show:
-                tc_sem_pred_ = tc_sem_pred.copy()
+                if 'tc_pred' in pred:
+                    tc_sem_pred_ = tc_sem_pred.copy()
+                else:
+                    tc_sem_pred_ = pred['sem_pred']
+                    bound = tc_sem_pred_ == len(self.CLASSES)
+                    tc_sem_pred_[tc_sem_pred_ > 0] = 1
+                    tc_sem_pred_[bound > 0] = 2
                 tc_sem_gt = get_tc_from_inst(inst_gt)
                 pred_collect = {'sem_pred': sem_pred, 'inst_pred': inst_pred, 'tc_sem_pred': tc_sem_pred_}
                 gt_collect = {'sem_gt': sem_gt, 'inst_gt': inst_gt, 'tc_sem_gt': tc_sem_gt}
@@ -235,7 +241,15 @@ class NucleiCoNICDataset(Dataset):
                     gt_collect.update({'dir_gt': dir_gt, 'ddm_gt': ddm_gt})
 
                 self.drawer = Drawer(show_folder, sem_palette=self.PALETTE)
-                metrics = {'imwAji': imw_aji}
+                metrics = {
+                    'imwAji': imw_aji,
+                    'pixel_TP': sem_pre_eval_res[0],
+                    'pixel_FP': sem_pre_eval_res[2],
+                    'pixel_FN': sem_pre_eval_res[3],
+                    'inst_TP': pq_pre_eval_res[0],
+                    'inst_FP': pq_pre_eval_res[1],
+                    'inst_FN': pq_pre_eval_res[2],
+                }
                 if 'dir_pred' in pred_collect and 'dir_gt' in gt_collect:
                     self.drawer.draw_direction(img_name, img_file_name, pred_collect, gt_collect, metrics)
                 else:
@@ -303,8 +317,9 @@ class NucleiCoNICDataset(Dataset):
     def model_agnostic_postprocess(self, pred):
         """model free post-process for both instance-level & semantic-level."""
         sem_id_list = list(np.unique(pred))
-        inst_pred = np.zeros_like(pred).astype(np.uint8)
+        inst_pred = np.zeros_like(pred).astype(np.int32)
         sem_pred = np.zeros_like(pred).astype(np.uint8)
+        cur = 0
         for sem_id in sem_id_list:
             # 0 is background semantic class.
             if sem_id == 0:
@@ -312,14 +327,14 @@ class NucleiCoNICDataset(Dataset):
             sem_id_mask = pred == sem_id
             # fill instance holes
             sem_id_mask = binary_fill_holes(sem_id_mask)
-            sem_id_mask_dila = morphology.dilation(sem_id_mask, selem=morphology.disk(2))
-            inst_pred[sem_id_mask > 0] = 1
-            sem_pred[sem_id_mask_dila > 0] = sem_id
-
-        # instance process & dilation
-        inst_pred = measure.label(inst_pred)
-        # if re_edge=True, dilation pixel length should be 2
-        inst_pred = morphology.dilation(inst_pred, selem=morphology.disk(2))
+            sem_id_mask = remove_small_objects(sem_id_mask, 5)
+            inst_sem_mask = measure.label(sem_id_mask)
+            inst_sem_mask = morphology.dilation(inst_sem_mask, selem=morphology.disk(2))
+            inst_sem_mask[inst_sem_mask > 0] += cur
+            inst_pred[inst_sem_mask > 0] = 0
+            inst_pred += inst_sem_mask
+            cur += len(np.unique(inst_sem_mask))
+            sem_pred[inst_sem_mask > 0] = sem_id
 
         return sem_pred, inst_pred
 
@@ -355,12 +370,16 @@ class NucleiCoNICDataset(Dataset):
         aji_pre_eval_results = ret_metrics.pop('aji_pre_eval_res')
         ret_metrics.update(pre_eval_to_aji(aji_pre_eval_results))
         bin_pq_pre_eval_results = ret_metrics.pop('bin_pq_pre_eval_res')
-        ret_metrics.update(pre_eval_to_bin_pq(bin_pq_pre_eval_results))
+        [ret_metrics.update(x) for x in pre_eval_to_bin_pq(bin_pq_pre_eval_results)]
         pq_pre_eval_results = ret_metrics.pop('pq_pre_eval_res')
-        ret_metrics.update(pre_eval_to_pq(pq_pre_eval_results))
+        [ret_metrics.update(x) for x in pre_eval_to_pq(pq_pre_eval_results)]
 
         total_inst_keys = ['bAji', 'mAji', 'bDQ', 'bSQ', 'bPQ', 'mDQ', 'mSQ', 'mPQ']
         total_inst_metrics = {}
+        total_analysis_keys = ['pq_bTP', 'pq_bFP', 'pq_bFN', 'pq_bIoU', 'pq_mTP', 'pq_mFP', 'pq_mFN', 'pq_mIoU']
+        total_analysis = {}
+        inst_analysis_keys = ['pq_TP', 'pq_FP', 'pq_FN', 'pq_IoU']
+        inst_analysis = {}
         inst_keys = ['Aji', 'DQ', 'SQ', 'PQ']
         inst_metrics = {}
         sem_keys = ['Dice', 'Precision', 'Recall']
@@ -377,6 +396,10 @@ class NucleiCoNICDataset(Dataset):
             elif key in sem_keys:
                 # remove background class
                 sem_metrics[key] = ret_metrics[key][1:]
+            elif key in total_analysis_keys:
+                total_analysis[key] = ret_metrics[key]
+            elif key in inst_analysis_keys:
+                inst_analysis[key] = ret_metrics[key][1:]
             else:
                 total_inst_metrics[key] = sum(ret_metrics[key]) / len(ret_metrics[key])
 
@@ -386,8 +409,9 @@ class NucleiCoNICDataset(Dataset):
             OrderedDict({sem_key: np.round(value * 100, 2)
                          for sem_key, value in sem_metrics.items()}))
         classes_metrics.update(
-            OrderedDict({sem_key: np.round(value * 100, 2)
-                         for sem_key, value in inst_metrics.items()}))
+            OrderedDict({inst_key: np.round(value * 100, 2)
+                         for inst_key, value in inst_metrics.items()}))
+        classes_metrics.update(OrderedDict({analysis_key: value for analysis_key, value in inst_analysis.items()}))
 
         # remove background class
         classes_metrics.update({'classes': self.CLASSES[1:]})
@@ -414,12 +438,18 @@ class NucleiCoNICDataset(Dataset):
         for key, val in inst_total_metrics.items():
             inst_total_table_data.add_column(key, [val])
 
+        total_analysis_table_data = PrettyTable()
+        for key, val in total_analysis.items():
+            total_analysis_table_data.add_column(key, [val])
+
         print_log('Per classes:', logger)
         print_log('\n' + classes_table_data.get_string(), logger=logger)
         print_log('Semantic Total:', logger)
         print_log('\n' + sem_total_table_data.get_string(), logger=logger)
         print_log('Instance Total:', logger)
         print_log('\n' + inst_total_table_data.get_string(), logger=logger)
+        print_log('Analysis Total:', logger)
+        print_log('\n' + total_analysis_table_data.get_string(), logger=logger)
 
         eval_results = {}
         # average results

tiseg/utils/inst_metrics.py
@@ -4,6 +4,8 @@ import numpy as np
 from skimage import measure
 from scipy.optimize import linear_sum_assignment
 
+from .misc import get_bounding_box
+
 
 def pre_eval_bin_aji(inst_pred, inst_gt):
     # make instance id contiguous
@@ -39,14 +41,28 @@ def pre_eval_bin_aji(inst_pred, inst_gt):
         if gt_id == 0:
             continue
         g_mask = gt_masks[gt_id]
-        pred_target_overlap = inst_pred[g_mask > 0]
+        rmin1, rmax1, cmin1, cmax1 = get_bounding_box(g_mask)
+        g_mask_crop = g_mask[rmin1:rmax1, cmin1:cmax1]
+        g_mask_crop = g_mask_crop.astype("int")
+        p_mask_crop = inst_pred[rmin1:rmax1, cmin1:cmax1]
+        pred_target_overlap = p_mask_crop[g_mask_crop > 0]
         pred_target_overlap_id = list(np.unique(pred_target_overlap))
         for pred_id in pred_target_overlap_id:
             if pred_id == 0:  # ignore
                 continue  # overlaping background
             p_mask = pred_masks[pred_id]
-            total = (g_mask + p_mask).sum()
-            intersect = (g_mask * p_mask).sum()
+
+            # crop region to speed up computation
+            rmin2, rmax2, cmin2, cmax2 = get_bounding_box(p_mask)
+            rmin = min(rmin1, rmin2)
+            rmax = max(rmax1, rmax2)
+            cmin = min(cmin1, cmin2)
+            cmax = max(cmax1, cmax2)
+            g_mask_union_crop = (g_mask[rmin:rmax, cmin:cmax] > 0).astype(np.uint8)
+            p_mask_union_crop = (p_mask[rmin:rmax, cmin:cmax] > 0).astype(np.uint8)
+            total = (g_mask_union_crop + p_mask_union_crop).sum()
+            intersect = (g_mask_union_crop * p_mask_union_crop).sum()
+            # print(total, intersect)
             pairwise_intersection[gt_id - 1, pred_id - 1] = intersect
             pairwise_union[gt_id - 1, pred_id - 1] = total - intersect
 
@@ -107,7 +123,11 @@ def pre_eval_aji(inst_pred, inst_gt, sem_pred, sem_gt, num_classes):
         p_mask = (inst_pred == p).astype(np.uint8)
 
         tp = np.sum(p_mask * sem_pred_one_hot, axis=(-2, -1))
-        belong_sem_id = np.argmax(tp)
+
+        if np.sum(tp[1:]) > 0:
+            belong_sem_id = np.argmax(tp[1:]) + 1
+        else:
+            belong_sem_id = 0
 
         if belong_sem_id not in pred_id_list_per_class:
             pred_id_list_per_class[belong_sem_id] = [p]
@@ -122,7 +142,11 @@ def pre_eval_aji(inst_pred, inst_gt, sem_pred, sem_gt, num_classes):
         g_mask = (inst_gt == g).astype(np.uint8)
 
         tp = np.sum(g_mask * sem_gt_one_hot, axis=(-2, -1))
-        belong_sem_id = np.argmax(tp)
+
+        if np.sum(tp[1:]) > 0:
+            belong_sem_id = np.argmax(tp[1:]) + 1
+        else:
+            belong_sem_id = 0
 
         if belong_sem_id not in gt_id_list_per_class:
             gt_id_list_per_class[belong_sem_id] = [g]
@@ -202,15 +226,29 @@ def pre_eval_bin_pq(inst_pred, inst_gt, match_iou=0.5):
     # caching pairwise iou
     for gt_id in gt_id_list[1:]:  # 0-th is background
         g_mask = gt_masks[gt_id]
-        pred_true_overlap = inst_pred[g_mask > 0]
-        pred_true_overlap_id = np.unique(pred_true_overlap)
-        pred_true_overlap_id = list(pred_true_overlap_id)
-        for pred_id in pred_true_overlap_id:
+
+        rmin1, rmax1, cmin1, cmax1 = get_bounding_box(g_mask)
+        g_mask_crop = g_mask[rmin1:rmax1, cmin1:cmax1]
+        g_mask_crop = g_mask_crop.astype("int")
+        p_mask_crop = inst_pred[rmin1:rmax1, cmin1:cmax1]
+        pred_gt_overlap = p_mask_crop[g_mask_crop > 0]
+        pred_gt_overlap_id = np.unique(pred_gt_overlap)
+        pred_gt_overlap_id = list(pred_gt_overlap_id)
+        for pred_id in pred_gt_overlap_id:
             if pred_id == 0:  # ignore
                 continue  # overlaping background
             p_mask = pred_masks[pred_id]
-            total = (g_mask + p_mask).sum()
-            inter = (g_mask * p_mask).sum()
+
+            # crop region to speed up computation
+            rmin2, rmax2, cmin2, cmax2 = get_bounding_box(p_mask)
+            rmin = min(rmin1, rmin2)
+            rmax = max(rmax1, rmax2)
+            cmin = min(cmin1, cmin2)
+            cmax = max(cmax1, cmax2)
+            g_mask_union_crop = g_mask[rmin:rmax, cmin:cmax]
+            p_mask_union_crop = p_mask[rmin:rmax, cmin:cmax]
+            total = (g_mask_union_crop + p_mask_union_crop).sum()
+            inter = (g_mask_union_crop * p_mask_union_crop).sum()
             iou = inter / (total - inter)
             pairwise_iou[gt_id - 1, pred_id - 1] = iou
 
@@ -280,7 +318,11 @@ def pre_eval_pq(inst_pred, inst_gt, sem_pred, sem_gt, num_classes):
         p_mask = (inst_pred == p).astype(np.uint8)
 
         tp = np.sum(p_mask * sem_pred_one_hot, axis=(-2, -1))
-        belong_sem_id = np.argmax(tp)
+
+        if np.sum(tp[1:]) > 0:
+            belong_sem_id = np.argmax(tp[1:]) + 1
+        else:
+            belong_sem_id = 0
 
         if belong_sem_id not in pred_id_list_per_class:
             pred_id_list_per_class[belong_sem_id] = [p]
@@ -295,7 +337,11 @@ def pre_eval_pq(inst_pred, inst_gt, sem_pred, sem_gt, num_classes):
         g_mask = (inst_gt == g).astype(np.uint8)
 
         tp = np.sum(g_mask * sem_gt_one_hot, axis=(-2, -1))
-        belong_sem_id = np.argmax(tp)
+
+        if np.sum(tp[1:]) > 0:
+            belong_sem_id = np.argmax(tp[1:]) + 1
+        else:
+            belong_sem_id = 0
 
         if belong_sem_id not in gt_id_list_per_class:
             gt_id_list_per_class[belong_sem_id] = [g]
@@ -318,8 +364,8 @@ def pre_eval_pq(inst_pred, inst_gt, sem_pred, sem_gt, num_classes):
         if sem_id == 0:
             pred_id_list = pred_id_list_per_class[sem_id]
             gt_id_list = gt_id_list_per_class[sem_id]
-            fp[sem_id] += sum([np.sum(inst_pred == pred_id) for pred_id in pred_id_list if pred_id != 0])
-            fn[sem_id] += sum([np.sum(inst_gt == gt_id) for gt_id in gt_id_list if gt_id != 0])
+            fp[sem_id] += len(pred_id_list)
+            fn[sem_id] += len(gt_id_list)
             continue
 
         if sem_id in pred_id_list_per_class and sem_id in gt_id_list_per_class:
@@ -338,10 +384,10 @@ def pre_eval_pq(inst_pred, inst_gt, sem_pred, sem_gt, num_classes):
         # NOTE: this part overall union is about semantic results mismatching between prediction & ground truth.
         elif sem_id in pred_id_list_per_class:
             pred_id_list = pred_id_list_per_class[sem_id]
-            fp[sem_id] += sum([np.sum(inst_pred == pred_id) for pred_id in pred_id_list if pred_id != 0])
+            fp[sem_id] += len(pred_id_list)
         elif sem_id in gt_id_list_per_class:
             gt_id_list = gt_id_list_per_class[sem_id]
-            fn[sem_id] += sum([np.sum(inst_gt == gt_id) for gt_id in gt_id_list if gt_id != 0])
+            fn[sem_id] += len(gt_id_list)
 
     return tp, fp, fn, iou
 
@@ -504,6 +550,7 @@ def pre_eval_to_bin_pq(pre_eval_results, nan_to_num=None):
 
     pq = dq * sq
 
+    analysis = {'pq_bTP': tp, 'pq_bFP': fp, 'pq_bFN': fn, 'pq_bIoU': np.round(iou, 2)}
     ret_metrics = {'bDQ': dq, 'bSQ': sq, 'bPQ': pq}
 
     if nan_to_num is not None:
@@ -511,7 +558,7 @@ def pre_eval_to_bin_pq(pre_eval_results, nan_to_num=None):
             {metric: np.nan_to_num(metric_value, nan=nan_to_num)
              for metric, metric_value in ret_metrics.items()})
 
-    return ret_metrics
+    return ret_metrics, analysis
 
 
 def pre_eval_to_pq(pre_eval_results, nan_to_num=None, reduce_zero_class_insts=True):
@@ -534,6 +581,7 @@ def pre_eval_to_pq(pre_eval_results, nan_to_num=None, reduce_zero_class_insts=Tr
     sq = iou / (tp + 1.0e-6)
     pq = dq * sq
 
+    analysis = {'pq_TP': tp, 'pq_FP': fp, 'pq_FN': fn, 'pq_IoU': np.round(iou, 2)}
     ret_metrics = {'DQ': dq, 'SQ': sq, 'PQ': pq}
 
     if reduce_zero_class_insts:
@@ -552,4 +600,4 @@ def pre_eval_to_pq(pre_eval_results, nan_to_num=None, reduce_zero_class_insts=Tr
             {metric: np.nan_to_num(metric_value, nan=nan_to_num)
              for metric, metric_value in ret_metrics.items()})
 
-    return ret_metrics
+    return ret_metrics, analysis

tiseg/utils/misc.py
@@ -90,9 +90,7 @@ def blend_image(image, mask, save_path=None, mask_palette=None, alpha=0.5):
 
     image_canvas = np.zeros_like(image, dtype=np.uint8)
     for i in range(3):
-        image_canvas[:, :,
-                     i] = alpha * image[:, :,
-                                        i] + (1 - alpha) * mask_canvas[:, :, i]
+        image_canvas[:, :, i] = alpha * image[:, :, i] + (1 - alpha) * mask_canvas[:, :, i]
 
     if save_path is not None:
         pillow_save(image_canvas, save_path=save_path)
@@ -110,3 +108,24 @@ def image_addition(arrays, save_path, palette=None):
     pillow_save(arrays, save_path=save_path, palette=palette)
 
     return arrays
+
+
+def get_bounding_box(img):
+    """Get the bounding box coordinates of a binary input- assumes a single object.
+
+    Args:
+        img: input binary image.
+
+    Returns:
+        bounding box coordinates
+
+    """
+    rows = np.any(img, axis=1)
+    cols = np.any(img, axis=0)
+    rmin, rmax = np.where(rows)[0][[0, -1]]
+    cmin, cmax = np.where(cols)[0][[0, -1]]
+    # due to python indexing, need to add 1 to max
+    # else accessing will be 1px in the box, not out
+    rmax += 1
+    cmax += 1
+    return [rmin, rmax, cmin, cmax]