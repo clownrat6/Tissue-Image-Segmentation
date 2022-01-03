84342a32881b26ccff191bc01cb1e6bda79cb7a3
fblogy <1050599136@qq.com>
2022-01-04 00:32:22 +0800

    add config

---
+++ configs/multi_task_cdnet/monuseg/multi_task_cdnet_vgg16_adam-lr5e-4_bs16_256x256_4k_monuseg_ac_area.py
@@ -0,0 +1,41 @@
+_base_ = [
+    '../../_base_/datasets/monuseg_w_dir.py',
+    '../../_base_/default_runtime.py',
+]
+
+# runtime settings
+runner = dict(type='IterBasedRunner', max_iters=4000)
+
+evaluation = dict(
+    interval=500,
+    metric='all',
+    save_best='mDice',
+    rule='greater',
+)
+checkpoint_config = dict(
+    by_epoch=False,
+    interval=500,
+)
+
+optimizer = dict(type='Adam', lr=0.0005, weight_decay=0.0005)
+optimizer_config = dict()
+
+# NOTE: poly learning rate decay
+# lr_config = dict(
+#     policy='poly', warmup='linear', warmup_iters=100, warmup_ratio=1e-6, power=1.0, min_lr=0.0, by_epoch=False)
+
+# NOTE: fixed learning rate decay
+lr_config = dict(policy='fixed', warmup=None, warmup_iters=100, warmup_ratio=1e-6, by_epoch=False)
+
+# model settings
+model = dict(
+    type='MultiTaskCDNetSegmentor',
+    # model training and testing settings
+    num_classes=2,
+    train_cfg=dict(use_ac=True, ac_w_area=True),
+    test_cfg=dict(
+        mode='whole',
+        rotate_degrees=[0, 90],
+        flip_directions=['none', 'horizontal', 'vertical', 'diagonal'],
+    ),
+)
