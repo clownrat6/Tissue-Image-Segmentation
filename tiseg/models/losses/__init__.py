b5acf706fc74eec5abf638b8edb4ee4db9b8e68b
sennnnn <201730271412@mail.scut.edu.cn>
2022-01-14 10:36:31 +0800

    Remove some model files.

--- tiseg/models/losses/__init__.py
+++ tiseg/models/losses/__init__.py
@@ -4,14 +4,27 @@ from .dice import Dice, dice, mdice, tdice
 from .dice_loss import DiceLoss, GeneralizedDiceLoss, MultiClassDiceLoss, BatchMultiClassDiceLoss
 from .iou import IoU, iou, miou, tiou
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
-from .surface_loss import SurfaceLoss
-from .focal_loss import FocalLoss2d, RobustFocalLoss2d
-from .level_set_loss import LevelsetLoss
-from .ac_loss import ActiveContourLoss
 
 __all__ = [
-    'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy', 'mask_cross_entropy', 'CrossEntropyLoss',
-    'reduce_loss', 'weight_reduce_loss', 'weighted_loss', 'DiceLoss', 'GeneralizedDiceLoss', 'IoU', 'iou', 'miou',
-    'Dice', 'dice', 'mdice', 'MultiClassDiceLoss', 'tdice', 'tiou', 'SurfaceLoss', 'BatchMultiClassDiceLoss',
-    'FocalLoss2d', 'RobustFocalLoss2d', 'LevelsetLoss', 'ActiveContourLoss'
+    'accuracy',
+    'Accuracy',
+    'cross_entropy',
+    'binary_cross_entropy',
+    'mask_cross_entropy',
+    'CrossEntropyLoss',
+    'reduce_loss',
+    'weight_reduce_loss',
+    'weighted_loss',
+    'DiceLoss',
+    'GeneralizedDiceLoss',
+    'IoU',
+    'iou',
+    'miou',
+    'Dice',
+    'dice',
+    'mdice',
+    'MultiClassDiceLoss',
+    'tdice',
+    'tiou',
+    'BatchMultiClassDiceLoss',
 ]
--- tiseg/models/segmentors/__init__.py
+++ tiseg/models/segmentors/__init__.py
@@ -3,11 +3,11 @@ from .unet import UNetSegmentor
 from .cdnet_voronoi import CDNetVoronoiSegmentor
 from .multi_task_unet import MultiTaskUNetSegmentor
 from .multi_task_cdnet import MultiTaskCDNetSegmentor
-from .regression_cdnet import RegCDNetSegmentor
-from .reg_degree_cdnet import RegDegreeCDNetSegmentor
-from .multi_task_cdnet_no_point import MultiTaskCDHeadNoPoint
 
 __all__ = [
-    'UNetSegmentor', 'CDNetSegmentor', 'CDNetVoronoiSegmentor', 'MultiTaskUNetSegmentor', 'MultiTaskCDNetSegmentor',
-    'RegCDNetSegmentor', 'RegDegreeCDNetSegmentor', 'MultiTaskCDHeadNoPoint'
+    'UNetSegmentor',
+    'CDNetSegmentor',
+    'CDNetVoronoiSegmentor',
+    'MultiTaskUNetSegmentor',
+    'MultiTaskCDNetSegmentor',
 ]
