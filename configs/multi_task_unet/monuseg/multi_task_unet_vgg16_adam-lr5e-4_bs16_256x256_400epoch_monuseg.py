69076830c3a9278cd46b91fcf6158110e8473b8f
fblogy <1050599136@qq.com>
2022-01-27 20:52:33 +0800

    fix config

--- configs/multi_task_unet/monuseg/multi_task_unet_vgg16_adam-lr5e-4_bs16_256x256_400epoch_monuseg.py
+++ configs/multi_task_unet/monuseg/multi_task_unet_vgg16_adam-lr5e-4_bs16_256x256_400epoch_monuseg.py
@@ -3,6 +3,51 @@ _base_ = [
     '../../_base_/default_runtime.py',
 ]
 
+# dataset settings
+dataset_type = 'NucleiMoNuSegDataset'
+data_root = 'data/monuseg'
+process_cfg = dict(
+    if_flip=True,
+    if_jitter=True,
+    if_elastic=True,
+    if_blur=True,
+    if_crop=True,
+    if_pad=True,
+    if_norm=False,
+    with_dir=False,
+    min_size=256,
+    max_size=2048,
+    resize_mode='fix',
+    edge_id=2,
+)
+data = dict(
+    samples_per_gpu=16,
+    workers_per_gpu=16,
+    train=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='train/c300',
+        ann_dir='train/c300',
+        split='only-train_t12_v4_train_c300.txt',
+        process_cfg=process_cfg),
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='train/c0',
+        ann_dir='train/c0',
+        split='only-train_t12_v4_test_c0.txt',
+        process_cfg=process_cfg),
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='train/c0',
+        ann_dir='train/c0',
+        split='only-train_t12_v4_test_c0.txt',
+        process_cfg=process_cfg),
+)
+
+
+
 epoch_iter = 12
 epoch_num = 400
 max_iters = epoch_iter * epoch_num
