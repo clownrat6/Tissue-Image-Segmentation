696c834eb55740c82612f42878137d8039f6459d
sennnnn <201730271412@mail.scut.edu.cn>
2022-03-31 20:13:46 +0800

    Support evaluation log.

--- tools/test.py
+++ tools/test.py
@@ -7,7 +7,7 @@ import mmcv
 import torch
 from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
 from mmcv.runner import get_dist_info, init_dist, load_checkpoint
-from mmcv.utils import DictAction
+from mmcv.utils import DictAction, get_logger
 
 from tiseg.apis import multi_gpu_test, single_gpu_test
 from tiseg.datasets import build_dataloader, build_dataset
@@ -45,6 +45,18 @@ def main():
     else:
         distributed = True
         init_dist(args.launcher, **cfg.dist_params)
+
+    if cfg.get('work_dir', None) is None:
+        # use config filename as default work_dir if cfg.work_dir is None
+        model_name = osp.dirname(args.config).replace('configs/', '')
+        config_name = osp.splitext(osp.basename(args.config))[0]
+        cfg.work_dir = f'./work_dirs/{model_name}/{config_name}'
+
+    # create work dir
+    eval_dir = osp.join(cfg.work_dir, 'eval')
+    mmcv.mkdir_or_exist(osp.abspath(eval_dir))
+    log_file = osp.join(eval_dir, 'eval.log')
+    logger = get_logger(name='TorchImageSeg', log_file=log_file, log_level=cfg.log_level)
 
     # build the model and load checkpoint
     model = build_segmentor(cfg.model, train_cfg=cfg.get('train_cfg'), test_cfg=cfg.get('test_cfg'))
@@ -90,13 +102,9 @@ def main():
 
         rank, _ = get_dist_info()
         if rank == 0:
-            ckpt_folder = osp.dirname(args.checkpoint)
-            dir_name = f'./{ckpt_folder}/test'
             ckpt_name = osp.splitext(osp.basename(args.checkpoint))[0]
-            eval_res, sotrage_res = dataset.evaluate(results, **eval_kwargs)
-            if not osp.exists(dir_name):
-                os.makedirs(dir_name, 0o775)
-            pickle.dump(sotrage_res, open(osp.join(dir_name, f'{ckpt_name}.p'), 'wb'))
+            eval_res, sotrage_res = dataset.evaluate(results, logger=logger, **eval_kwargs)
+            pickle.dump(sotrage_res, open(osp.join(eval_dir, f'{ckpt_name}.p'), 'wb'))
 
 
 if __name__ == '__main__':
